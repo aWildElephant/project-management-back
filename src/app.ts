913a238fc21e4965501836658e1ed1d8710d5a2c
Etienne Girard <etienne.g.girard@gmail.com>
2021-04-04 11:35:45 +0200

    Task CRUD

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,30 @@
+import cors from 'cors'
 import express from 'express';
-import { sequelize } from './model';
+import { sequelize, Task } from './model';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Coucou');
-});
+app.use(express.json())
+
+app.use(cors())
+
+app.post('/task', async (req, res) => {
+    const task = await Task.create(req.body)
+
+    console.log("Saved %s", JSON.stringify(req.body));
+
+    res.send(task)
+})
+
+app.get("/task/:id", async (req, res) => {
+    const task = await Task.findByPk(req.params.id)
+
+    res.send(task)
+})
 
 sequelize.authenticate()
+    .then(() => sequelize.sync())
     .then(() => {
         console.log("Connected to database")
 
--- src/model.ts
+++ src/model.ts
@@ -1,3 +1,22 @@
-import { Sequelize } from 'sequelize'
+import { DataTypes, Sequelize } from 'sequelize'
 
 export const sequelize = new Sequelize('postgres://pguser:password@localhost:5432/project-management')
+
+export const Task = sequelize.define("task", {
+    id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT
+    }
+}, {
+    createdAt: "creationTimestamp",
+    updatedAt: "modificationTimestamp",
+    underscored: true
+})
